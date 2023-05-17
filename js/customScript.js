71387c9f7e2f7a040099b5bb51b1e4db2bbde8f3
KB <kinga.boros20@gmail.com>
2023-05-17 11:34:56 +0300

    scroll is working now

--- js/customScript.js
+++ js/customScript.js
@@ -1,70 +1,105 @@
 // horizontal scrolling
-
-let scrollInterval = null; // Variable to store the scroll interval
+let scrollTimeout = null; // Variable to store the scroll timeout
+const sections = document.querySelectorAll('section');
 
 function scrollH(e) {
   if (window.innerWidth >= 1000) {
     e = window.event || e;
     let delta = Math.max(-1, Math.min(1, e.deltaY || -e.detail));
     const mainContainer = document.querySelector('.mainContainer');
-    const sections = document.querySelectorAll('section');
     const currentScrollLeft = mainContainer.scrollLeft;
 
-    clearInterval(scrollInterval); // Clear any existing scroll interval
+    clearTimeout(scrollTimeout); // Clear any existing scroll timeout
 
-    // Calculate the target position based on the current scroll position and delta
-    const targetSection = Array.from(sections).find((section) => {
-      const sectionStart = section.offsetLeft;
-      const sectionEnd = sectionStart + section.offsetWidth;
-      return delta > 0
-        ? sectionStart > currentScrollLeft
-        : sectionEnd < currentScrollLeft;
-    });
+    let targetSection = null;
+    let targetPosition = null;
+
+    if (delta > 0) {
+      // Scrolling to the right
+      for (let i = 0; i < sections.length; i++) {
+        const section = sections[i];
+        const sectionStart = section.offsetLeft;
+
+        if (sectionStart > currentScrollLeft) {
+          targetSection = section;
+          targetPosition = sectionStart;
+          break;
+        }
+      }
+    } else {
+      // Scrolling to the left
+      for (let i = sections.length - 1; i >= 0; i--) {
+        const section = sections[i];
+        const sectionEnd = section.offsetLeft + section.offsetWidth;
+
+        if (sectionEnd <= currentScrollLeft + mainContainer.clientWidth) {
+          targetSection = section;
+          targetPosition = sectionEnd - mainContainer.clientWidth;
+          break;
+        }
+      }
+    }
 
     if (targetSection) {
-      const targetPosition =
-        delta > 0
-          ? targetSection.offsetLeft
-          : targetSection.offsetLeft +
-            targetSection.offsetWidth -
-            mainContainer.clientWidth;
+      const scrollDelay = 100; // Adjust the scroll delay as needed
 
-      // Start the scroll interval
-      scrollInterval = setInterval(() => {
-        const distance = targetPosition - mainContainer.scrollLeft;
-        const scrollStep =
-          delta > 0 ? Math.ceil(distance / 10) : Math.floor(distance / 10);
+      scrollTimeout = setTimeout(() => {
+        const animateScroll = () => {
+          const distance = targetPosition - mainContainer.scrollLeft;
+          const scrollStep =
+            delta > 0 ? Math.ceil(distance / 10) : Math.floor(distance / 10);
 
-        mainContainer.scrollLeft += scrollStep;
+          mainContainer.scrollLeft += scrollStep;
 
-        // Stop the scroll interval when reaching the target position
-        if (
-          (delta > 0 && mainContainer.scrollLeft >= targetPosition) ||
-          (delta < 0 && mainContainer.scrollLeft <= targetPosition)
-        ) {
-          clearInterval(scrollInterval);
+          // Stop the scroll animation when reaching the target position
+          if (
+            (delta > 0 && mainContainer.scrollLeft >= targetPosition) ||
+            (delta < 0 && mainContainer.scrollLeft <= targetPosition)
+          ) {
+            clearTimeout(scrollTimeout);
 
-          // Update the section ID in the URL hash
-          const sectionID = targetSection.getAttribute('id');
-          if (sectionID) {
-            window.location.hash = sectionID;
+            // Update the section ID in the URL hash
+            const sectionID = targetSection.getAttribute('id');
+            if (sectionID) {
+              window.location.hash = sectionID;
 
-            // Reset the hash if the target section has an ID of "home"
-            if (sectionID === 'home') {
-              window.location.hash = '';
+              // Reset the hash if the target section has an ID of "home"
+              if (sectionID === 'home') {
+                window.location.hash = '';
+              }
             }
+
+            // Add activeSection class to the visible section
+            sections.forEach((section) => {
+              if (section === targetSection) {
+                section.classList.add('activeSection');
+              } else {
+                section.classList.remove('activeSection');
+              }
+            });
+
+            // Add active class to the menu links
+            const menuLinks = document.querySelectorAll('.desktopMenu a');
+            menuLinks.forEach((link) => {
+              const linkTarget = link.getAttribute('href');
+              if (linkTarget === `#${sectionID}`) {
+                link.classList.add('active');
+              } else {
+                link.classList.remove('active');
+              }
+            });
+
+            // Check if scrolled to the promo section and call liftPromotions
+            if (sectionID === 'promo') {
+              liftPromotions('promo');
+            }
+          } else {
+            requestAnimationFrame(animateScroll);
           }
+        };
 
-          // Add activeSection class to the visible section
-          sections.forEach((section) => {
-            if (section === targetSection) {
-              section.classList.add('activeSection');
-            } else {
-              section.classList.remove('activeSection');
-            }
-          });
-        }
-      }, 10); // Adjust the scroll interval speed as needed (in milliseconds)
+        animateScroll();
+      }, scrollDelay);
     }
 
     mainContainer.classList.remove('mobileMainContainer');
@@ -79,6 +114,150 @@ function scrollH(e) {
     }
   }
 }
+
+// Add click event listener to menu links
+const menuLinks = document.querySelectorAll('.desktopMenu a');
+menuLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const targetSectionID = link.getAttribute('href').substring(1);
+    const targetSection = document.getElementById(targetSectionID);
+
+    // Scroll to the target section
+    if (targetSection) {
+      const mainContainer = document.querySelector('.mainContainer');
+      const targetPosition = targetSection.offsetLeft;
+
+      clearTimeout(scrollTimeout);
+      mainContainer.scrollLeft = targetPosition;
+
+      // Update the section ID in the URL hash
+      if (targetSectionID === 'home') {
+        window.location.hash = '';
+      } else {
+        window.location.hash = targetSectionID;
+      }
+
+      // Add activeSection class to the visible section
+      sections.forEach((section) => {
+        if (section === targetSection) {
+          section.classList.add('activeSection');
+        } else {
+          section.classList.remove('activeSection');
+        }
+      });
+
+      // Add active class to the menu links
+      menuLinks.forEach((menuLink) => {
+        if (menuLink === link) {
+          menuLink.classList.add('active');
+        } else {
+          menuLink.classList.remove('active');
+        }
+      });
+
+      // Check if scrolled to the promo section and call liftPromotions
+      if (targetSectionID === 'promo') {
+        liftPromotions('promo');
+      }
+    }
+  });
+});
+
+// let scrollInterval = null; // Variable to store the scroll interval
+
+// function scrollH(e) {
+//   if (window.innerWidth >= 1000) {
+//     e = window.event || e;
+//     let delta = Math.max(-1, Math.min(1, e.deltaY || -e.detail));
+//     const mainContainer = document.querySelector('.mainContainer');
+//     const sections = document.querySelectorAll('section');
+//     const currentScrollLeft = mainContainer.scrollLeft;
+
+//     clearInterval(scrollInterval); // Clear any existing scroll interval
+
+//     let targetSection = null;
+
+//     sections.forEach((section) => {
+//       const sectionStart = section.offsetLeft;
+//       const sectionEnd = sectionStart + section.offsetWidth;
+
+//       if (
+//         (delta > 0 &&
+//           sectionStart > currentScrollLeft &&
+//           (!targetSection || sectionStart < targetSection.offsetLeft)) ||
+//         (delta < 0 &&
+//           sectionEnd < currentScrollLeft &&
+//           (!targetSection || sectionEnd > targetSection.offsetLeft))
+//       ) {
+//         targetSection = section;
+//       }
+//     });
+
+//     if (targetSection) {
+//       const targetPosition =
+//         delta > 0
+//           ? targetSection.offsetLeft -
+//             mainContainer.clientWidth +
+//             targetSection.offsetWidth
+//           : targetSection.offsetLeft;
+
+//       const scrollSpeed = 10; // Adjust the scroll speed as needed
+
+//       // Start the scroll interval
+//       scrollInterval = setInterval(() => {
+//         const distance =
+//           delta > 0
+//             ? targetPosition - mainContainer.scrollLeft
+//             : mainContainer.scrollLeft - targetPosition;
+
+//         // Check if the scroll step will overshoot the target position
+//         if (
+//           (delta > 0 &&
+//             mainContainer.scrollLeft + scrollSpeed >= targetPosition) ||
+//           (delta < 0 &&
+//             mainContainer.scrollLeft - scrollSpeed <= targetPosition)
+//         ) {
+//           mainContainer.scrollLeft = targetPosition;
+//           clearInterval(scrollInterval);
+
+//           // Update the section ID in the URL hash
+//           const sectionID = targetSection.getAttribute('id');
+//           if (sectionID) {
+//             window.location.hash = sectionID;
+
+//             // Reset the hash if the target section has an ID of "home"
+//             if (sectionID === 'home') {
+//               window.location.hash = '';
+//             }
+//           }
+
+//           // Add activeSection class to the visible section
+//           sections.forEach((section) => {
+//             if (section === targetSection) {
+//               section.classList.add('activeSection');
+//             } else {
+//               section.classList.remove('activeSection');
+//             }
+//           });
+//         } else {
+//           mainContainer.scrollLeft += delta > 0 ? scrollSpeed : -scrollSpeed;
+//         }
+//       }, 5); // Adjust the scroll interval speed as needed (in milliseconds)
+//     }
+
+//     mainContainer.classList.remove('mobileMainContainer');
+//   } else {
+//     const mainContainer = document.querySelector('.mainContainer');
+//     mainContainer.classList.add('mobileMainContainer');
+//     updateHash(mainContainer.scrollLeft);
+
+//     const hash = window.location.hash;
+//     if (hash === '#home') {
+//       history.replaceState(null, null, window.location.pathname);
+//     }
+//   }
+// }
 
 //////////////////////////////
 
@@ -327,8 +506,6 @@ function updateDots() {
 createDots();
 showSlide();
 
-///// promotions curtain effect
-
 // Lift Promotions
 const liftPromotions = (sectionId) => {
   const windowWidth = window.innerWidth;
@@ -353,11 +530,5 @@ const liftPromotions = (sectionId) => {
 
 // Attach liftPromotions to the scroll event
 window.addEventListener('scroll', function () {
-  liftPromotions('promo');
-});
-
-// Attach scrollH to the wheel event
-window.addEventListener('wheel', function (e) {
-  scrollH(e);
   liftPromotions('promo');
 });
