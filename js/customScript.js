9c3230c18e75d882dcf875de4401058033fdc4dc
kinga <kinga.boros20@gmail.com>
2023-05-19 12:46:29 +0300

    mobile screen

--- js/customScript.js
+++ js/customScript.js
@@ -435,3 +435,15 @@ const liftPromotions = (sectionId) => {
 window.addEventListener('scroll', function () {
   liftPromotions('promo');
 });
+
+// hamburger menu
+
+document.addEventListener('DOMContentLoaded', function () {
+  const navbarToggler = document.querySelector('.navbar-toggler');
+  const navbarMenu = document.querySelector('.navbar-collapse');
+
+  navbarToggler.addEventListener('click', function () {
+    navbarMenu.classList.toggle('show');
+    navbarMenu.classList.toggle('mobileMenu');
+  });
+});
